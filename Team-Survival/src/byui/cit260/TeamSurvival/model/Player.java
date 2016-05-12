bc0a9b1e3eb30f05c0c7813f5a9bb9cf8ab648e0
prich <prich@DESKTOP-2DUTJ77>
2016-05-12 10:36:49 -0600

    5-12-2016 Initial commit of project

---
+++ Team-Survival/src/byui/cit260/TeamSurvival/model/Player.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.TeamSurvival.model;
+import java.io.Serializable;
+import java.util.Objects;
+/**
+ *
+ * @author prich
+ */
+public class Player implements Serializable {
+    
+    //class instance variable 
+    private String name;
+    private int lessMove; 
+
+    public Player() {
+    }
+    
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLessMove() {
+        return lessMove;
+    }
+
+    public void setLessMove(int lessMove) {
+        this.lessMove = lessMove;
+    }   
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 29 * hash + Objects.hashCode(this.name);
+        hash = 29 * hash + this.lessMove;
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + ", lessMove=" + lessMove + '}';
+    }
+
+    
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (this.lessMove != other.lessMove) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+   
+    
+}
+
