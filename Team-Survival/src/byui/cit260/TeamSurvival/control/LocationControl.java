b765e8416377cbb117f90b36f53cd679c186cb12
Aboli <Aboli@10.18.149.241>
2016-05-27 10:59:14 -0600

    5 - 27 - 2016 - test file modification

--- Team-Survival/src/byui/cit260/TeamSurvival/control/LocationControl.java
+++ Team-Survival/src/byui/cit260/TeamSurvival/control/LocationControl.java
@@ -42,8 +42,6 @@ public class LocationControl {
          }
          
          double distance = (double) Math.sqrt( Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2) );
-         
-       
          return distance;
      }
         
