58b67912842529c51b454f07c0fb011b4b58f2e6
Aboli <Aboli@10.18.149.241>
2016-05-27 01:15:44 -0600

    5 - 27 - 2016 - test file modification

--- Team-Survival/test/byui/cit260/TeamSurvival/control/LocationControlTest.java
+++ Team-Survival/test/byui/cit260/TeamSurvival/control/LocationControlTest.java
@@ -35,8 +35,7 @@ public class LocationControlTest {
         System.out.println("calcArea");
         
         //Test case 1
-        
-        System.out.println("Test case 1");
+        System.out.println("test case 1");
 
         double radius = 4.0;
         LocationControl instance = new LocationControl();
@@ -44,26 +43,28 @@ public class LocationControlTest {
         double result = instance.calcArea(radius);
         assertEquals(expResult, result, 0.01);
         
+        
+        
         //Test case 2
+        System.out.println("test case 2");
 
-        System.out.println("Test case 2");
-
-        radius = 0.0;
+        radius = 0;
         expResult = -1;
         result = instance.calcArea(radius);
         assertEquals(expResult, result, 0.01);
+        
         
         
         //Test case 3
-
-        System.out.println("Test case 3");
-
+        System.out.println("test case 3");
         radius = -1;
         expResult = -1;
         result = instance.calcArea(radius);
+        System.out.println(result);
+        System.out.println(expResult);
         assertEquals(expResult, result, 0.01);
         
-        // Test case 4
+                // Test case 4
         System.out.println("Test case 4");
 
         radius = 121;
@@ -98,7 +99,44 @@ public class LocationControlTest {
         
         
     }
-    
-    
+
+    /**
+     * Test of getFinalResult method, of class LocationControl.
+     */
+    @Test
+    public void testGetFinalResult() {
+        System.out.println("getFinalResult");
+        
+        
+        // Test case 1A
+        System.out.println("Test case 1A");        
+        double area = 3;
+        double number = 92;
+        LocationControl instance = new LocationControl();
+        double expResult = 95;
+        double result = instance.getFinalResult(area, number);
+        System.out.println(result);
+        System.out.println(expResult);
+        assertEquals(expResult, result, 0.01);
+        
+        //test case 2A
+        System.out.println("Test case 2A");
+        area = -12;
+        number = 270;
+        expResult = -1;
+        result = instance.getFinalResult(area, number);
+        assertEquals(expResult, result, 0.0);
+        
+        
+        //test case 3A
+        System.out.println("Test case 3A");
+        area = 50.27;
+        number = 40;
+        expResult = -1;
+        result = instance.getFinalResult(area, number);
+        assertEquals(expResult, result, 0.0);
+        
+        
+    }
     
 }
